59bcfeda08eaee6b78148a3b9d271c4cbe98e45d
JarkkoKaa <jarkko.kaartinen@edu.savonia.fi>
2019-05-06 12:23:13 +0300

    Add project files.

---
+++ CsharpAlgorithms/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CsharpAlgorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

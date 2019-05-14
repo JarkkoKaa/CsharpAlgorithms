f0a8e4bcb5c95a29817431598073320bde67fd00
JarkkoKaa <jarkko.kaartinen@edu.savonia.fi>
2019-05-14 11:22:38 +0300

    quicksort

--- CsharpAlgorithms/Program.cs
+++ CsharpAlgorithms/Program.cs
@@ -19,24 +19,36 @@ namespace CsharpAlgorithms
             list.DisplayAll();
         }
 
-        static void BubbleSort(int[] numbers)
+        static void BubbleSort(int[] values)
         { 
             BubbleSort<int> bsort = new BubbleSort<int>();
-            bsort.Sort(numbers);
-            foreach (int num in numbers)
+            var sorted = bsort.Sort(values);
+        }
+
+        static void MergeSort(int[] values)
+        {
+            MergeSort<int> mergeSort = new MergeSort<int>();
+            var sorted = mergeSort.Sort(values);
+            for (int i = 0; i < sorted.Length; i++)
             {
-                Console.WriteLine(num);
+                Console.WriteLine(sorted.GetValue(i));
             }
+        }
+
+        static void QuickSort(char[] values)
+        {
+            QuickSort<char> quickSort = new QuickSort<char>();
+            var sorted = quickSort.Sort(values, 0, values.Length - 1);
         }
 
         static void Main(string[] args)
         {
-            int[] numbers = new int[] { 9, 4, 6, 7, 8, 3, 2, 1, 5 };
-            // int[] numbers = new int[] { 2, 4, 1, 3, 5, 7, 8 };
+            //int[] values = new int[] { 9, 4, 6, 7, 8, 3, 2, 1, 5 };
+            char[] values = new char[] { 'c', 'd', 'b', 'a' };
             // LinkedList();
-            // BubbleSort(numbers);
-            MergeSort<int> mergeSort = new MergeSort<int>();
-            var sorted = mergeSort.Sort(numbers);
+            // BubbleSort(values);
+            // MergeSort(values);
+            QuickSort(values);
         }
     }
 }

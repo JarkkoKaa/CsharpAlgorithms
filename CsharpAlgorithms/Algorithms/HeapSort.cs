f890102d0a5154fa5a260e45beef2416bc9c0f81
JarkkoKaa <jarkko.kaartinen@edu.savonia.fi>
2019-07-10 22:35:50 +0300

    Heapsort

--- CsharpAlgorithms/Algorithms/HeapSort.cs
+++ CsharpAlgorithms/Algorithms/HeapSort.cs
@@ -12,8 +12,14 @@ namespace CsharpAlgorithms.Algorithms
             compare = Comparer<T>.Default;
         }
 
+        /// <summary>
+        /// Heap sort
+        /// </summary>
+        /// <param name="array"></param>
+        /// <returns></returns>
         public T[] Sort(T[] array)
         {
+            // Build heap
             for (int i = array.Length / 2 - 1; i >= 0; i--)
             {
                 Heapify(array, array.Length, i);
@@ -31,17 +37,21 @@ namespace CsharpAlgorithms.Algorithms
             return array;
         }
 
+        /// <summary>
+        /// Arrange elements
+        /// </summary>
         private T[] Heapify(T[] array,int len,int i)
         {
-            int largest = i;
-            int left = 2 * i + 1;
-            int right = left + 1;
+            int largest = i; // init largest as root
+            int left = 2 * i + 1; // left child
+            int right = left + 1; // right child
 
             if (left < array.Length && compare.Compare(array[left], array[largest]) < 0)
                 largest = left;
             if (right < array.Length && compare.Compare(array[right], array[largest]) < 0)
                 largest = right;
 
+            // if largest is not the root
             if (largest != i)
             {
                 T temp = array[i];
